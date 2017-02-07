76a6054e36b9143484946e1f5db486d524851f41
Ehimare Agboneni <eagbnoneni@albany.edu>
2017-02-06 22:23:08 -0500

    Added a contructor to Item class, created new class ItemCreate and made main method to create the item

--- Item.java
+++ Item.java
@@ -1,4 +1,20 @@
 public class Item {
 
+  private String nameOfItem;
+  private int weightOfItem;
+  
+  //Item constructor, takes in string and int for the item
+  public Item(String name, int weight){
+  
+    nameOfItem = name;
+    weightOfitem = weight;
+  }
+  
 
+  
+  
+  
+  
+
+  
 }
---
+++ ItemCreate.java
@@ -0,0 +1,8 @@
+public class ItemCreate{
+
+public static void main(String[]args)
+{
+  //Creates the specified item based on set variables
+  
+}
+}
