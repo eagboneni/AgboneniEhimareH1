df7bca85ac50fc371caa3785bbc4ffa8ad06e318
Ehimare Agboneni <eagbnoneni@albany.edu>
2017-02-11 20:08:10 -0500

    Created Armor class and the contructor, added javadoc comments for methods in each class

---
+++ Armor.java
@@ -0,0 +1,15 @@
+public class Armor{
+
+  private int defense;
+  private int durability;
+  
+  /**
+   * Constructor for armor class takes in 
+   * String and 3 int values 
+   */
+  public Armor(String name, int weight, int defense, int durability){
+   super(name, weight);
+   defense = def;
+   durability = dura;
+  }
+}
--- Item.java
+++ Item.java
@@ -3,35 +3,52 @@ public class Item {
   private String nameOfItem;
   private int weightOfItem;
   
-  
+  /**
+   * Constructor for item class takes in 
+   * String and int  
+   */
   public Item(String name, int weight){
   //Item constructor, takes in string and int for the item
     nameOfItem = name;
     weightOfItem = weight;
   }
   
-
+      /**
+   * The set method for getting the name of the item
+   * @param name
+   */
   public void setNameOfItem(String name){
   //Stores value for name of the item
     nameOfItem = name;
   }
   
+      /**
+   * The set method for getting the weight of the item 
+   * @param weight
+   */
   public void setWeightOfItem(int weight){
   //Stores value for the weight of the item 
     weightOfItem = weight;
   }
-  
+    /**
+   *The get method that returns the name of the item
+   * @return nameOfItem
+   */
   public String getNameOfItem(){
   //Returns the item's name
     return nameOfItem;
   }
-  
+    /**
+   *The get method that returns the value for weight of the item 
+   * @return weightOfItem
+   */
   public int getWeightOfItem(){
   //Returns the weight of item
     return weightOfItem;
   }
-  
-  
+   /**
+   * The examine method displays a string that describes the item
+   */
    public void examine(){
   //Displays discription of the item with it's name and weight
     System.out.println("The " + this.getNameOfItem() + " weighs " + this.getWeightOfItem() + " pounds" );
